01636b2388235ccc05543168dc29f402071f6393
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2020-06-19 01:23:27 +0800

    V4

---
+++ Bymgp_LV1_Deemo/Assets/Scrips/packges.cs
@@ -0,0 +1,17 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class packges : MonoBehaviour
+{
+    public GameObject UI_Start;
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            UI_Start.SetActive(false);
+        }
+    }
+}
