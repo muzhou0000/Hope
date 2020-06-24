7c8fc3615cf976a1757f9c3730a1b981ec9025af
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2020-06-25 01:13:05 +0800

    移動跟鏡頭

--- Bymgp_LV1_Deemo/Assets/Scrips/packges.cs
+++ Bymgp_LV1_Deemo/Assets/Scrips/packges.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class packges : MonoBehaviour
 {
     public GameObject UI_Start;
+    public int menu;
     void Update()
     {
         if (Input.anyKeyDown)
         {
             UI_Start.SetActive(false);
+            menu = 1;
         }
     }
 }
