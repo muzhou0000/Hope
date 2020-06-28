0f71645c8638a095adfbd546adb970dff740815e
Oningyo <55180582+Oningyo@users.noreply.github.com>
2020-06-28 20:35:03 +0800

    2020_06/28
    攝影機修整

--- Bymgp_LV1_Deemo/Assets/Scrips/packges.cs
+++ Bymgp_LV1_Deemo/Assets/Scrips/packges.cs
@@ -11,9 +11,10 @@ public class packges : MonoBehaviour
     public Animator STB_ani;
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown)//如果按下任意鈕
         {
             STB_ani.SetBool("1", true);
+            //等17秒後執行下面那個程式
             Invoke("endUIStart", 17);
         }
     }
