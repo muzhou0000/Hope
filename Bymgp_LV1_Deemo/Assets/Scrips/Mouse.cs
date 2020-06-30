6e7e4a71a72a1d5c2be821f9818a5187fd6ecc3c
maoche <maoche001@gmail.com>
2020-07-01 01:58:03 +0800

    7/1

--- Bymgp_LV1_Deemo/Assets/Scrips/Mouse.cs
+++ Bymgp_LV1_Deemo/Assets/Scrips/Mouse.cs
@@ -1,11 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
 
 public class Mouse : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +14,6 @@ public class Mouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 }
